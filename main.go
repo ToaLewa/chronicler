14f1387c0e50964ce0aabee5575d60f7585824e2
Kolton Kulis <kkulis@Koltons-MacBook-Pro.local>
2026-08-19 12:53:22 -0500

    Format date

--- main.go
+++ main.go
@@ -42,9 +42,11 @@ func createFirst() {
 func main() {
 
 	now := time.Now()
-	year, month, day := now.Date()
+	// year, month, day := now.Date()
 
-	fmt.Println(strconv.Itoa(year) + strconv.Itoa(int(month)) + strconv.Itoa(day))
+	nowF := now.Format("2006-01-02")
+
+	fmt.Println(nowF)
 
 	// Read the current directory (".")
 	entries, err := os.ReadDir("/Users/kkulis/Documents/atg/chrono/")
