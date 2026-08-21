38c4da8e038dd4bcce9713e08d29b266ae43a2f0
Toa Lewa <kooltone16@gmail.com>
2026-08-21 15:26:01 -0500

    Check args

--- main.go
+++ main.go
@@ -51,62 +51,70 @@ func createFirst(fileName string) {
 	fmt.Println(fileName)
 }
 
+func hasArg() bool {
+	return len(os.Args) > 1
+}
+
 func main() {
 
 	now := time.Now()
 	nowF := now.Format("2006-01-02")
-	// year, month, day := now.Date()
-
-	oFile := ChronoFile{
-		Logs: []Log{
-			{
-				Date: nowF,
-				Entries: []Entry{
-					{
-						Time: "11:00",
-						Text: "Blah blah blah",
+	if hasArg() {
+		// year, month, day := now.Date()
+		oFile := ChronoFile{
+			Logs: []Log{
+				{
+					Date: nowF,
+					Entries: []Entry{
+						{
+							Time: "11:00",
+							Text: "Blah blah blah",
+						},
 					},
 				},
 			},
-		},
+		}
+
+		b, _ := json.Marshal(oFile)
+
+		os.WriteFile("test.chro", b, 0666)
+
+		fmt.Println("Write file")
+
+	} else {
+		fmt.Println(nowF)
+
+		// Read the current directory (".")
+		entries, err := os.ReadDir("/Users/kkulis/Documents/atg/chrono/")
+		fileCount := len(entries)
+		check(err)
+
+		if fileCount > 0 {
+			fmt.Println("Files found")
+			fmt.Println(strconv.Itoa(fileCount))
+
+			fileName := "chronicle-" + nowF + ".md"
+			createFirst(fileName)
+
+		}
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		//
+		// for _, entry := range entries {
+		// 	// Output the name and whether it's a directory
+		// 	fmt.Printf("Name: %s | IsDir: %t\n", entry.Name(), entry.IsDir())
+		// }
+
+		path := "/Users/kkulis/Documents/atg/chrono/chronicle-2026-08-18.md"
+		dat, err := os.ReadFile(path)
+		check(err)
+
+		fmt.Println(dat)
+		// f, err := os.Open(path)
+		// check(err)
+
+		// fmt.Print(string(dat))
+		// fmt.Println("Test")
 	}
-
-	b, _ := json.Marshal(oFile)
-
-	os.WriteFile("test.chro", b, 0666)
-
-	fmt.Println(nowF)
-
-	// Read the current directory (".")
-	entries, err := os.ReadDir("/Users/kkulis/Documents/atg/chrono/")
-	fileCount := len(entries)
-	check(err)
-
-	if fileCount > 0 {
-		fmt.Println("Files found")
-		fmt.Println(strconv.Itoa(fileCount))
-
-		fileName := "chronicle-" + nowF + ".md"
-		createFirst(fileName)
-
-	}
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//
-	// for _, entry := range entries {
-	// 	// Output the name and whether it's a directory
-	// 	fmt.Printf("Name: %s | IsDir: %t\n", entry.Name(), entry.IsDir())
-	// }
-
-	path := "/Users/kkulis/Documents/atg/chrono/chronicle-2026-08-18.md"
-	dat, err := os.ReadFile(path)
-	check(err)
-
-	fmt.Println(dat)
-	// f, err := os.Open(path)
-	// check(err)
-
-	// fmt.Print(string(dat))
-	// fmt.Println("Test")
 }
