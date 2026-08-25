9fcbe6c63f8ac04090a25591610d1db01e71b7f1
Toa Lewa <kooltone16@gmail.com>
2026-08-24 21:56:39 -0500

    Remove unused code in main

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"chronicler/internal/chrono"
-	"chronicler/internal/timepieces"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func check(e error) {
@@ -94,41 +92,5 @@ func main() {
 			fmt.Println("Write file")
 		}
 
-	} else {
-		timePieces := timepieces.GetCurrent()
-		fmt.Println(timePieces.DateString)
-
-		// Read the current directory (".")
-		entries, err := os.ReadDir("/Users/kkulis/Documents/atg/chrono/")
-		fileCount := len(entries)
-		check(err)
-
-		if fileCount > 0 {
-			fmt.Println("Files found")
-			fmt.Println(strconv.Itoa(fileCount))
-
-			fileName := "chronicle-" + timePieces.DateString + ".md"
-			createFirst(fileName)
-
-		}
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-		//
-		// for _, entry := range entries {
-		// 	// Output the name and whether it's a directory
-		// 	fmt.Printf("Name: %s | IsDir: %t\n", entry.Name(), entry.IsDir())
-		// }
-
-		path := "/Users/kkulis/Documents/atg/chrono/chronicle-2026-08-18.md"
-		dat, err := os.ReadFile(path)
-		check(err)
-
-		fmt.Println(dat)
-		// f, err := os.Open(path)
-		// check(err)
-
-		// fmt.Print(string(dat))
-		// fmt.Println("Test")
 	}
 }
